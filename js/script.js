45a91d29d9981c2cbe8cafbf2078e2d1b6361111
Chris Canipe <chris.canipe@wsj.com>
2016-05-03 20:39:20 -0500

    fixed label position, set style in css for active team/line on hover

--- js/script.js
+++ js/script.js
@@ -1,20 +1,25 @@
-d3.selection.prototype.moveToFront=function(){
-    return this.each(function(){
+d3.selection.prototype.moveToFront = function() {
+    return this.each(function() {
         this.parentNode.appendChild(this)
     });
 };
-d3.selection.prototype.moveToBack= function(){
-    return this.each(function(){
+d3.selection.prototype.moveToBack = function() {
+    return this.each(function() {
         var firstChild = this.parentNode.firstChild;
-        if (firstChild){
+        if (firstChild) {
             this.parentNode.insertBefore(this, firstChild);
         }
     });
 };
 
-var margin = {top: 20, right:80, bottom: 30, left: 50};
-    width = $(".chart").width() - margin.left - margin.right;
-    height = $(".chart").height() - margin.top - margin.bottom;
+var margin = {
+    top: 20,
+    right: 80,
+    bottom: 30,
+    left: 50
+};
+width = $(".chart").width() - margin.left - margin.right;
+height = $(".chart").height() - margin.top - margin.bottom;
 
 var parseDate = d3.time.format("%Y").parse;
 
@@ -34,10 +39,16 @@ var yAxis = d3.svg.axis()
 
 var line = d3.svg.line()
     .interpolate("basis")
-    .defined(function(d) { return d.Time > 0; })
-    .x(function(d) {return x(d.Year); })
-    .y(function(d) {return y(d.Time); })
-    
+    .defined(function(d) {
+        return d.Time > 0;
+    })
+    .x(function(d) {
+        return x(d.Year);
+    })
+    .y(function(d) {
+        return y(d.Time);
+    })
+
 
 
 
@@ -54,119 +65,148 @@ var svg = d3.select(".chart").append("svg")
 d3.csv("data/mlb_time.csv", function(error, data) {
     if (error) throw error;
 
-    color.domain(d3.keys(data[0]).filter(function(key) { return key !== "Year"; }));
+    color.domain(d3.keys(data[0]).filter(function(key) {
+        return key !== "Year";
+    }));
 
     data.forEach(function(d) {
         d.Year = parseDate(d.Year);
     });
 
-    var teams = color.domain().map(function(name){
+    var teams = color.domain().map(function(name) {
         return {
-            
+
 
             name: name,
-            values: data.map(function(d){
+            values: data.map(function(d) {
                 var minutes = +d[name];
                 if (!minutes) {
                     minutes = null;
-                } 
+                }
 
-                return {Year: d.Year, Time: minutes};
-                
-            })    
+                return {
+                    Year: d.Year,
+                    Time: minutes
+                };
+
+            })
         };
     });
-x.domain(d3.extent(data, function(d) {return d.Year;}));
-y.domain([
-    d3.min(teams, function(c) { return d3.min(c.values, function(v) {return v.Time;});}),
-    d3.max(teams, function(c) { return d3.max(c.values, function(v) {return v.Time;});})
+    x.domain(d3.extent(data, function(d) {
+        return d.Year;
+    }));
+    y.domain([
+        d3.min(teams, function(c) {
+            return d3.min(c.values, function(v) {
+                return v.Time;
+            });
+        }),
+        d3.max(teams, function(c) {
+            return d3.max(c.values, function(v) {
+                return v.Time;
+            });
+        })
     ]);
 
 
 
-svg.append("g")
-    .attr("class", "x-axis")
-    .attr("transform", "translate(0," + height + ")")
-    .call(xAxis);
+    svg.append("g")
+        .attr("class", "x-axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis);
 
-svg.append("g")
-    .attr("class", "y-axis")
-    .call(yAxis)
-    .append("text")
-    .attr("transform", "rotate(-90)")
-    .attr("y", 6)
-    .attr("dy", ".71em")
-    .style("text-anchor", "end")
-    .text("Time (Minutes)");
+    svg.append("g")
+        .attr("class", "y-axis")
+        .call(yAxis)
+        .append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 6)
+        .attr("dy", ".71em")
+        .style("text-anchor", "end")
+        .text("Time (Minutes)");
 
 
-var team = svg.selectAll(".team")
-    .data(teams)
-    .enter().append("g")
-    .attr("class", "team")
-    .attr("team", function(d){
-        return d.name;
+    var team = svg.selectAll(".team")
+        .data(teams)
+        .enter().append("g")
+        .attr("class", "team")
+        .attr("team", function(d) {
+            return d.name;
         })
-    .on("mouseover", function(d){
-        d3.select(this).moveToFront();
-        d3.select(this).classed("active", true);
-    })
-    .on("mouseout", function(d){
-        d3.select(this).moveToBack();
-        d3.select(this).classed("active", false);
-    })    
-team.append("path")
-    .attr("class", function(d) {
-        return "line "+d.name;
-    })
-    .attr("d", function(d) { return line(d.values); })
-    
+        .on("mouseover", function(d) {
+            d3.select(this).moveToFront();
+            d3.select(this).classed("active", true);
+        })
+        .on("mouseout", function(d) {
+            d3.select(this).moveToBack();
+            d3.select(this).classed("active", false);
+        })
 
-team.append("text")
-    .datum(function(d) {return {name: d.name, value: d.values[d.values.length - 1]}; })
-    .attr("transform", function(d) { return "translate(" + x(d.value.Year) + "," + y(d.value.Time) + ")";})
-    .attr("x", 500)
-    .attr("dy", ".35em")
-    .text(function(d) {return d.name; });
-team.selectAll(".dot")
-    .data(function(d){
-        return d.values != null;
-    })
-    .enter().append("circle")
-    .attr("class", "dot")
-    .attr("cx", function(d){
-        return x(d.Year);
-    })
-    .attr("cy", function(d){
-        return y(d.value.Time) != null;})
-    .attr("r", 5)
-    .on("mouseover", function(d){
-        var displayDate = (d.Year);
-        var displayTime = (d.value.Time);
+        
+    team.append("path")
+        .attr("class", function(d) {
+            return "line " + d.name;
+        })
+        .attr("d", function(d) {
+            return line(d.values);
+        })
 
-        $(".tt").html(
-            "<div class='team_name'>"+d.name+"</div>"+
-            "<div class='year_play'>"+displayDate+": </div>"+
-            "<div clss='time_average'>"+displayTime+": </div>"
+
+    team.append("text")
+        .datum(function(d) {
+            return {
+                name: d.name,
+                value: d.values[d.values.length - 1]
+            };
+        })
+        .attr("transform", function(d) {
+            return "translate(" + x(d.value.Year) + "," + y(d.value.Time) + ")";
+        })
+        //.attr("x", 500)
+        .attr("dy", ".35em")
+        .text(function(d) {
+            return d.name;
+        });
+    team.selectAll(".dot")
+        .data(function(d) {
+            return d.values != null;
+        })
+        .enter().append("circle")
+        .attr("class", "dot")
+        .attr("cx", function(d) {
+            return x(d.Year);
+        })
+        .attr("cy", function(d) {
+            return y(d.value.Time) != null;
+        })
+        .attr("r", 5)
+        .on("mouseover", function(d) {
+            var displayDate = (d.Year);
+            var displayTime = (d.value.Time);
+
+            $(".tt").html(
+                "<div class='team_name'>" + d.name + "</div>" +
+                "<div class='year_play'>" + displayDate + ": </div>" +
+                "<div clss='time_average'>" + displayTime + ": </div>"
             )
-        $(".tt").show();
+            $(".tt").show();
 
-        d3.select(this).style("opacity", 1);
-    })
-    .on("mousemove", function(d){
-        var xPos = d3.mouse(this)[0] + margin.left + 10;
-        var yPos = d3.mouse(this)[1] + margin.top +10;
-
-        $(".tt").css({
-            "left": xPos + "px",
-            "top": yPos + "px"
+            d3.select(this).style("opacity", 1);
         })
-    })
-    .on("mouseout", function(d){
-        d3.select(this).style("opacity", 0);
-        $(".tt").hide();
-    });
+        .on("mousemove", function(d) {
+            var xPos = d3.mouse(this)[0] + margin.left + 10;
+            var yPos = d3.mouse(this)[1] + margin.top + 10;
+
+            $(".tt").css({
+                "left": xPos + "px",
+                "top": yPos + "px"
+            })
+        })
+        .on("mouseout", function(d) {
+            d3.select(this).style("opacity", 0);
+            $(".tt").hide();
+        });
 });
 
-   
-//first class line, second class team name, 
+
+//first class line, second class team name,
